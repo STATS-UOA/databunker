64c961c34ce5622c20fa92f66511568d890b4a23
cmjt <cmjonestodd@gmail.com>
2021-07-26 15:29:30 +1200

    initial commit w two datasets

---
+++ R/data.R
@@ -0,0 +1,47 @@
+#' Behavioral response of the Mediterranean lizard to smoke
+#'
+#' Data from a study conducted to investigate whether the Mediterranean lizard (Psammodromus algirus)
+#' could recognise the threat of fire by detecting smoke and triggering a behavioral
+#' response that enhances survival in fire-prone ecosystems.
+#'
+#' @docType data
+#'
+#' @usage data(lizards)
+#'
+#' @format An object of class \code{"tbl_df"} with 216 rows and 13 columns
+#'
+#' @keywords datasets
+#'
+#' @references Lola Álvarez-Ruiz, Josabel Belliure, Juli G Pausas, Fire-driven behavioral
+#' response to smoke in a Mediterranean lizard, Behavioral Ecology, 2021
+#'(\href{doi:10.5061/dryad.xpnvx0kdv}{Behavioral Ecology})
+#'
+#' @source \href{https://doi.org/10.1093/beheco/arab010}{Behavioral Ecology}
+#'
+#' @examples
+#' data(lizards)
+#'\donttest{lizards}
+"lizards"
+
+#' Recorded howls of the Indian wolf
+#'
+#' Data from a survey conducted recording the howls of Indian wolves.
+#'
+#' @docType data
+#'
+#' @usage data(howls)
+#'
+#' @format An object of class \code{"tbl_df"} with 132 rows and 14 columns
+#'
+#' @keywords datasets
+#'
+#' @references Sadhukhan, S., Root-Gutteridge, H. & Habib, B. Identifying unknown Indian wolves by their
+#' distinctive howls: its potential as a non-invasive survey method. Sci Rep 11, 7309 (2021)
+#'(\href{https://www.nature.com/articles/s41598-021-86718-w}{Scientific Reports})
+#'
+#' @source \href{https://github.com/bhlabwii/wolf_howlID}{GitHub}
+#'
+#' @examples
+#' data(howls)
+#'\donttest{howls}
+"howls"
---
+++ README.md
@@ -0,0 +1,18 @@
+## Overview
+
+A package that contains a number of datasets used in teaching BIOSCI 738 at the University of Auckland. To install run
+
+```
+devtools::install_github("STATS-UOA/databunker")
+library(databunker)
+```
+
+## Datasets
+
++ [Lola Álvarez-Ruiz, Josabel Belliure, Juli G Pausas, Fire-driven behavioral response to smoke in a Mediterranean lizard, *Behavioral Ecology*, (2021)](https://doi.org/10.1093/beheco/arab010)
++ [Sadhukhan, S., Root-Gutteridge, H. & Habib, B. Identifying unknown Indian wolves by their distinctive howls: its potential as a non-invasive survey method. *Scientific Reports* 11, 7309 (2021)](https://www.nature.com/articles/s41598-021-86718-w)
+
+
+## Copyright
+
+All files are (C)Copyright by the University of Auckland unless explicitly stated otherwise. Distrubution without prior written permission is prohibited.
